e50e5c14cbc0bfec36504803e2bf9b04b0dea8b3
davep3rrett <david.m.perrett@gmail.com>
2019-10-14 02:29:02 -0400

    Modifications to echo3
    Book exercises:
     - 1.1
     - 1.2

--- gopl.io/ch1/echo3/echo3.go
+++ gopl.io/ch1/echo3/echo3.go
@@ -9,4 +9,12 @@ import (
 func main() {
 	fmt.Println(strings.Join(os.Args[1:], " "))
 	fmt.Println(os.Args[1:])
+
+	// Include name of invoking command - Ex 1.1
+	fmt.Println(strings.Join(os.Args[0:], " "))
+
+	// Print index and value of args, 1 per line - Ex 1.2
+	for i, arg := range os.Args[1:] {
+		fmt.Println(i, arg)
+	}
 }
